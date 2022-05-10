d44f9081e5961e1dca6b4b31f1b4ab20436e15cf
LBLZR_ <avojtus@gmail.com>
2022-05-11 00:21:00 +0200

    Update prepare_data.py

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,10 +1,23 @@
 import os
-import csv
 import itertools
 import functools
 import tensorflow as tf
 import numpy as np
 import array
+
+from torchtext.data import get_tokenizer
+
+tokenizer = get_tokenizer("basic_english")
+
+def del_all_flags(FLAGS):
+    flags_dict = FLAGS._flags()    
+    keys_list = [keys for keys in flags_dict]    
+    for keys in keys_list:
+        FLAGS.__delattr__(keys)
+
+del_all_flags(tf.flags.FLAGS)
+
+tf.compat.v1.flags.DEFINE_string('f','','')
 
 tf.flags.DEFINE_integer(
   "min_word_frequency", 5, "Minimum frequency of words in the vocabulary")
@@ -13,7 +26,7 @@ tf.flags.DEFINE_integer("max_sentence_len", 160, "Maximum Sentence Length")
 
 tf.flags.DEFINE_string(
   "input_dir", os.path.abspath("./data"),
-  "Input directory containing original CSV data files (default = './data')")
+  "Input directory containing original data files (default = './data')")
 
 tf.flags.DEFINE_string(
   "output_dir", os.path.abspath("./data"),
@@ -21,23 +34,32 @@ tf.flags.DEFINE_string(
 
 FLAGS = tf.flags.FLAGS
 
-TRAIN_PATH = os.path.join(FLAGS.input_dir, "train.csv")
-VALIDATION_PATH = os.path.join(FLAGS.input_dir, "valid.csv")
-TEST_PATH = os.path.join(FLAGS.input_dir, "test.csv")
+TRAIN_PATH = os.path.join(FLAGS.input_dir, "train.txt")
+VALIDATION_PATH = os.path.join(FLAGS.input_dir, "valid.txt")
+TEST_PATH = os.path.join(FLAGS.input_dir, "test.txt")
 
 def tokenizer_fn(iterator):
-  return (x.split(" ") for x in iterator)
+  return (tokenizer(x) if x != '<EOS>' else ['<EOS>'] for x in iterator)
 
-def create_csv_iter(filename):
-  """
-  Returns an iterator over a CSV file. Skips the header.
-  """
-  with open(filename) as csvfile:
-    reader = csv.reader(csvfile)
-    # Skip the header
-    next(reader)
-    for row in reader:
-      yield row
+
+def read_support_dialogues(f_path):
+    dialogues = []
+    with open(f_path, encoding='utf-8') as f:
+        for line in f.readlines():
+            
+            
+            item_arr = line.strip().split('\t')
+            
+            if len(item_arr) < 2:
+                pass
+            
+            context = [u.strip() for u in item_arr[1:-1]]
+            response = item_arr[-1].strip()
+            label = int(item_arr[0].strip())
+            
+            dialogues.append((context, response, label))
+
+    return dialogues
 
 
 def create_vocab(input_iter, min_frequency):
@@ -57,30 +79,28 @@ def transform_sentence(sequence, vocab_processor):
   """
   Maps a single sentence into the integer vocabulary. Returns a python array.
   """
-  return next(vocab_processor.transform([sequence])).tolist()
+  return next(vocab_processor.transform(sequence)).tolist()
+
+def insert_eos(utterances):
+  tr = []
+  for u in utterances:
+    tr.append(u)
+    tr.append('<EOS>')
+  return tr
 
 
-def create_text_sequence_feature(fl, sentence, sentence_len, vocab):
-  """
-  Writes a sentence to FeatureList protocol buffer
-  """
-  sentence_transformed = transform_sentence(sentence, vocab)
-  for word_id in sentence_transformed:
-    fl.feature.add().int64_list.value.extend([word_id])
-  return fl
-
-
-def create_example_train(row, vocab):
+def create_train(row, vocab):
   """
   Creates a training example for the Ubuntu Dialog Corpus dataset.
   Returnsthe a tensorflow.Example Protocol Buffer object.
   """
-  context, utterance, label = row
-  context_transformed = transform_sentence(context, vocab)
-  utterance_transformed = transform_sentence(utterance, vocab)
-  context_len = len(next(vocab._tokenizer([context])))
-  utterance_len = len(next(vocab._tokenizer([utterance])))
+  context, response, label = row
   label = int(float(label))
+  #context = insert_eos(context)
+  context_transformed = transform_sentence(context, vocab)
+  utterance_transformed = transform_sentence(response, vocab)
+  context_len = len(next(vocab._tokenizer(context)))
+  utterance_len = len(next(vocab._tokenizer(response)))
 
   # New Example
   example = tf.train.Example()
@@ -92,47 +112,63 @@ def create_example_train(row, vocab):
   return example
 
 
-def create_example_test(row, vocab):
-  """
-  Creates a test/validation example for the Ubuntu Dialog Corpus dataset.
-  Returnsthe a tensorflow.Example Protocol Buffer object.
-  """
-  context, utterance = row[:2]
-  distractors = row[2:]
-  context_len = len(next(vocab._tokenizer([context])))
-  utterance_len = len(next(vocab._tokenizer([utterance])))
-  context_transformed = transform_sentence(context, vocab)
-  utterance_transformed = transform_sentence(utterance, vocab)
 
-  # New Example
-  example = tf.train.Example()
-  example.features.feature["context"].int64_list.value.extend(context_transformed)
-  example.features.feature["utterance"].int64_list.value.extend(utterance_transformed)
-  example.features.feature["context_len"].int64_list.value.extend([context_len])
-  example.features.feature["utterance_len"].int64_list.value.extend([utterance_len])
-
-  # Distractor sequences
-  for i, distractor in enumerate(distractors):
-    dis_key = "distractor_{}".format(i)
-    dis_len_key = "distractor_{}_len".format(i)
-    # Distractor Length Feature
-    dis_len = len(next(vocab._tokenizer([distractor])))
-    example.features.feature[dis_len_key].int64_list.value.extend([dis_len])
-    # Distractor Text Feature
-    dis_transformed = transform_sentence(distractor, vocab)
-    example.features.feature[dis_key].int64_list.value.extend(dis_transformed)
-  return example
-
-
-def create_tfrecords_file(input_filename, output_filename, example_fn):
+def create_tfrecords_train_file(rows, output_filename, vocab):
   """
   Creates a TFRecords file for the given input data and
   example transofmration function
   """
   writer = tf.python_io.TFRecordWriter(output_filename)
   print("Creating TFRecords file at {}...".format(output_filename))
-  for i, row in enumerate(create_csv_iter(input_filename)):
-    x = example_fn(row)
+  for row in rows:
+    x = create_train(row, vocab)
+    writer.write(x.SerializeToString())
+  writer.close()
+  print("Wrote to {}".format(output_filename))
+
+
+def create_test(rows, vocab):
+  """
+  Creates a test/validation example for the Ubuntu Dialog Corpus dataset.
+  Returnsthe a tensorflow.Example Protocol Buffer object.
+  """
+
+  # New Example
+  example = tf.train.Example()
+
+  for i, r in enumerate(rows):
+    context, response, label = r
+    #context = insert_eos(context)
+    label = int(float(label))
+
+    context_transformed = transform_sentence(context, vocab)
+    utterance_transformed = transform_sentence(response, vocab)
+    context_len = len(next(vocab._tokenizer(context)))
+    utterance_len = len(next(vocab._tokenizer(response)))
+
+    example.features.feature["context"].int64_list.value.extend(context_transformed)
+    example.features.feature["context_len"].int64_list.value.extend([context_len])
+
+    if label == 1:
+      example.features.feature["utterance"].int64_list.value.extend(utterance_transformed)
+      example.features.feature["utterance_len"].int64_list.value.extend([utterance_len])
+    else:
+      dis_key = "distractor_{}".format(i)
+      dis_len_key = "distractor_{}_len".format(i)
+      example.features.feature[dis_len_key].int64_list.value.extend([utterance_len])
+      example.features.feature[dis_key].int64_list.value.extend(utterance_transformed)
+  return example
+
+def create_tfrecords_test_file(rows, output_filename, vocab, n=10):
+  """
+  Creates a TFRecords file for the given input data and
+  example transofmration function
+  """
+  writer = tf.python_io.TFRecordWriter(output_filename)
+  print("Creating TFRecords file at {}...".format(output_filename))
+  for i in range(0, len(rows), n):
+    batch = rows[i:i+n]
+    x = create_test(batch, vocab)
     writer.write(x.SerializeToString())
   writer.close()
   print("Wrote to {}".format(output_filename))
@@ -145,17 +181,22 @@ def write_vocabulary(vocab_processor, outfile):
   vocab_size = len(vocab_processor.vocabulary_)
   with open(outfile, "w") as vocabfile:
     for id in range(vocab_size):
-      word =  vocab_processor.vocabulary_._reverse_mapping[id]
+      word = vocab_processor.vocabulary_._reverse_mapping[id]
       vocabfile.write(word + "\n")
   print("Saved vocabulary to {}".format(outfile))
 
 
 if __name__ == "__main__":
   print("Creating vocabulary...")
-  input_iter = create_csv_iter(TRAIN_PATH)
-  input_iter = (x[0] + " " + x[1] for x in input_iter)
-  vocab = create_vocab(input_iter, min_frequency=FLAGS.min_word_frequency)
+  train_dialogues = read_support_dialogues(TRAIN_PATH)
+  valid_dialogues = read_support_dialogues(VALIDATION_PATH)
+  test_dialogues = read_support_dialogues(TEST_PATH)
+  all_utterances = [' '.join(c) + ' ' + r for c, r, _ in train_dialogues + valid_dialogues + test_dialogues]
+  #all_utterances.extend(['<EOS>', '<EOS>', '<EOS>', '<EOS>', '<EOS>', '<EOS>', '<EOS>', '<EOS>', '<EOS>', '<EOS>', '<EOS>']) # nepozeraj sa sem
+
+  vocab = create_vocab(all_utterances, min_frequency=FLAGS.min_word_frequency)
   print("Total vocabulary size: {}".format(len(vocab.vocabulary_)))
+
 
   # Create vocabulary.txt file
   write_vocabulary(
@@ -165,19 +206,16 @@ if __name__ == "__main__":
   vocab.save(os.path.join(FLAGS.output_dir, "vocab_processor.bin"))
 
   # Create validation.tfrecords
-  create_tfrecords_file(
-      input_filename=VALIDATION_PATH,
-      output_filename=os.path.join(FLAGS.output_dir, "validation.tfrecords"),
-      example_fn=functools.partial(create_example_test, vocab=vocab))
+  create_tfrecords_test_file(
+      valid_dialogues,
+      output_filename=os.path.join(FLAGS.output_dir, "validation.tfrecords"), vocab=vocab)
 
   # Create test.tfrecords
-  create_tfrecords_file(
-      input_filename=TEST_PATH,
-      output_filename=os.path.join(FLAGS.output_dir, "test.tfrecords"),
-      example_fn=functools.partial(create_example_test, vocab=vocab))
+  create_tfrecords_test_file(
+      test_dialogues,
+      output_filename=os.path.join(FLAGS.output_dir, "test.tfrecords"), vocab=vocab)
 
   # Create train.tfrecords
-  create_tfrecords_file(
-      input_filename=TRAIN_PATH,
-      output_filename=os.path.join(FLAGS.output_dir, "train.tfrecords"),
-      example_fn=functools.partial(create_example_train, vocab=vocab))
+  create_tfrecords_train_file(
+      train_dialogues,
+      output_filename=os.path.join(FLAGS.output_dir, "train.tfrecords"), vocab=vocab)
